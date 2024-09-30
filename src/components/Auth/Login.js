cde79f12531986ae3cc3a3f1b54f3e92de206796
shaik younus <younus123shaik@gmail.com>
2024-09-30 16:41:58 +0530

    second commit

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -6,10 +6,23 @@ const Login = () => {
         nav("/home")
     }
   return (
-    <div>
-        Login
-        <button onClick={handleSubmit}>Submit</button>
-    </div>
+    <div className='loginbox'>
+    <form className='loginform log' style={{height:"300px"}} onSubmit={handleSubmit}>
+      <div style={{width:"100%",textAlign:"center"}}>
+        <h1>Login Here</h1>
+      </div>
+      <label>Email address</label>
+      <div style={{display:"flex", flexDirection:"column"}}>
+      <input type='email' required/>
+      <small>we'll never share your email with anyone</small>
+      </div>
+      <label>Password</label>
+      <input type='password' required/>
+      <div style={{display:"flex",justifyContent:"center",width:"100%"}}>
+      <button type='submit' className='btnlogin' style={{padding:"10px"}}>Submit</button>
+      </div>
+    </form>
+</div>
   )
 }
 
--- src/components/Auth/Signin.js
+++ src/components/Auth/Signin.js
@@ -6,9 +6,35 @@ const Signin = () => {
         nav('/login')
     }
   return (
-    <div>
-        Signin
-        <button onClick={handleSubmit}>Submit</button>
+    <div className='loginbox'>
+        <form className='loginform' onSubmit={handleSubmit}>
+          <div style={{width:"100%",textAlign:"center"}}>
+            <h1>Register Here</h1>
+          </div>
+          <label>Name</label>
+          <input type='text' required/>
+          <label>Email address</label>
+          <div style={{display:"flex", flexDirection:"column"}}>
+          <input type='email' required/>
+          <small>we'll never share your email with anyone</small>
+          </div>
+          <label>Password</label>
+          <input type='password' required/>
+          <div className='radio'>
+          <label>Role:</label>
+          <div>
+          <input type='radio'/>
+          <label>Trainer</label>
+          </div>
+          <div>
+          <input type='radio'/>
+          <label>Student</label>
+          </div>
+          </div>
+          <div style={{display:"flex",justifyContent:"center",width:"100%"}}>
+          <button type='submit' className='btnsignin' style={{padding:"10px"}}>Submit</button>
+          </div>
+        </form>
     </div>
   )
 }
